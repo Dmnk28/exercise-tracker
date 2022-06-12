d3038a4f0b71d8e479f54a7afa3bf65354264f97
Dominik Oesterle <dominik.oesterle@posteo.de>
2022-06-12 08:23:23 +0200

    Adjusted path in faviconhandler

--- handlers/faviconHandler.js
+++ handlers/faviconHandler.js
@@ -1,7 +1,10 @@
-const path = require('path');
+import path from 'path';
+import * as url from 'url';
+
+const dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const faviconHandler = (req, res) => {
-    res.sendFile(path.join(__dirname, '/../public/favicon.ico'))
+    res.sendFile(path.join(dirname, '/../public/favicon.ico'))
 }
 
-module.exports = faviconHandler;
+export default faviconHandler;
